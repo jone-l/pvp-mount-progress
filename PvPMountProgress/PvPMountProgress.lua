04769a3a4a0771dc1b7e7fdb48e12e563785ef76
Jone Lima <jone.lima@gmail.com>
2022-11-04 07:48:08 +0100

    Added solo queue and updated to 10.0.0

--- PvPMountProgress/PvPMountProgress.lua
+++ PvPMountProgress/PvPMountProgress.lua
@@ -1,36 +1,42 @@
 hooksecurefunc(PVPQueueFrame.HonorInset.RatedPanel.SeasonRewardFrame, "UpdateTooltip", function(self)
 	
-	local achievementID = self:GetAchievementID();
+	local achievementID = self:GetAchievementID()
 	if not achievementID then
-		return;
+		return
 	end
 	if GetAchievementNumCriteria(achievementID) == 0 then
-		return;
+		return
 	end
 		
-	EmbeddedItemTooltip:SetOwner(self, "ANCHOR_RIGHT");
-	GameTooltip_SetTitle(EmbeddedItemTooltip, PVP_SEASON_REWARD);
+	EmbeddedItemTooltip:SetOwner(self, "ANCHOR_RIGHT")
+	GameTooltip_SetTitle(EmbeddedItemTooltip, PVP_SEASON_REWARD)
 
-	local criteriaString, criteriaType, completed, quantity, reqQuantity, charName, flags, assetID, quantityString = GetAchievementCriteriaInfo(achievementID, 1);
+	local criteriaString, criteriaType, completed, quantity, reqQuantity, charName, flags, assetID, quantityString = 
+		GetAchievementCriteriaInfo(achievementID, 1)
+	local victoriesLeftSolo = math.ceil((reqQuantity-quantity)/50)
 	local victoriesLeft2v2 = math.ceil((reqQuantity-quantity)/10)
 	local victoriesLeft3v3 = math.ceil((reqQuantity-quantity)/30)
 	local victoriesLeftrbg = math.ceil((reqQuantity-quantity)/60)
 
 	if criteriaString then
 		if completed then
-			GameTooltip_AddColoredLine(EmbeddedItemTooltip, GOAL_COMPLETED, GREEN_FONT_COLOR);
+			GameTooltip_AddColoredLine(EmbeddedItemTooltip, GOAL_COMPLETED, GREEN_FONT_COLOR)
 		else
-			local wordWrap = true;
-			GameTooltip_AddNormalLine(EmbeddedItemTooltip, criteriaString, wordWrap);
-			local roundToNearestInteger = true;
-			local progressString = format("%s (%d / %d)", FormatPercentage(quantity / reqQuantity, roundToNearestInteger), quantity, reqQuantity)
-			GameTooltip_ShowProgressBar(EmbeddedItemTooltip, 0, reqQuantity, quantity, progressString);
-			GameTooltip_AddNormalLine(EmbeddedItemTooltip, format("Victories required:\n    2v2:  %d\n    3v3:  %d\n    RBG: %d", victoriesLeft2v2, victoriesLeft3v3, victoriesLeftrbg), wordWrap)
-			local rewardItemID = C_AchievementInfo.GetRewardItemID(achievementID);
+			local wordWrap = true
+			local roundToNearestInteger = true
+			local rewardItemID = C_AchievementInfo.GetRewardItemID(achievementID)
+			local progressString = format("%s (%d / %d)", FormatPercentage(quantity / reqQuantity, roundToNearestInteger), 
+				quantity, reqQuantity)
+
+			GameTooltip_AddNormalLine(EmbeddedItemTooltip, criteriaString, wordWrap)
+			GameTooltip_ShowProgressBar(EmbeddedItemTooltip, 0, reqQuantity, quantity, progressString)
+			GameTooltip_AddNormalLine(EmbeddedItemTooltip, 
+				format("Victories required:\n   Solo:  %d\n    2v2:  %d\n    3v3:  %d\n    RBG: %d", 
+				victoriesLeftSolo, victoriesLeft2v2, victoriesLeft3v3, victoriesLeftrbg), wordWrap)
 			if rewardItemID then
-				GameTooltip_AddBlankLinesToTooltip(EmbeddedItemTooltip, 1);
-				GameTooltip_AddNormalLine(EmbeddedItemTooltip, REWARD, wordWrap);
-				EmbeddedItemTooltip_SetItemByID(EmbeddedItemTooltip.ItemTooltip, rewardItemID);
+				GameTooltip_AddBlankLinesToTooltip(EmbeddedItemTooltip, 1)
+				GameTooltip_AddNormalLine(EmbeddedItemTooltip, REWARD, wordWrap)
+				EmbeddedItemTooltip_SetItemByID(EmbeddedItemTooltip.ItemTooltip, rewardItemID)
 			end
 		end
 	end	
