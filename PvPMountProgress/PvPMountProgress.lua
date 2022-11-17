13b4111eaff449a0bd3e4fd12e3417426bc522c9
Jone Lima <jone.lima@gmail.com>
2022-11-17 08:57:35 +0100

    Updated TOC to 10.0.2

--- PvPMountProgress/PvPMountProgress.lua
+++ PvPMountProgress/PvPMountProgress.lua
@@ -1,5 +1,5 @@
 hooksecurefunc(PVPQueueFrame.HonorInset.RatedPanel.SeasonRewardFrame, "UpdateTooltip", function(self)
-	
+
 	local achievementID = self:GetAchievementID()
 	if not achievementID then
 		return
@@ -7,11 +7,11 @@ hooksecurefunc(PVPQueueFrame.HonorInset.RatedPanel.SeasonRewardFrame, "UpdateToo
 	if GetAchievementNumCriteria(achievementID) == 0 then
 		return
 	end
-		
+
 	EmbeddedItemTooltip:SetOwner(self, "ANCHOR_RIGHT")
 	GameTooltip_SetTitle(EmbeddedItemTooltip, PVP_SEASON_REWARD)
 
-	local criteriaString, criteriaType, completed, quantity, reqQuantity, charName, flags, assetID, quantityString = 
+	local criteriaString, criteriaType, completed, quantity, reqQuantity, charName, flags, assetID, quantityString =
 		GetAchievementCriteriaInfo(achievementID, 1)
 	local victoriesLeftSolo = math.ceil((reqQuantity-quantity)/50)
 	local victoriesLeft2v2 = math.ceil((reqQuantity-quantity)/10)
@@ -25,13 +25,13 @@ hooksecurefunc(PVPQueueFrame.HonorInset.RatedPanel.SeasonRewardFrame, "UpdateToo
 			local wordWrap = true
 			local roundToNearestInteger = true
 			local rewardItemID = C_AchievementInfo.GetRewardItemID(achievementID)
-			local progressString = format("%s (%d / %d)", FormatPercentage(quantity / reqQuantity, roundToNearestInteger), 
+			local progressString = format("%s (%d / %d)", FormatPercentage(quantity / reqQuantity, roundToNearestInteger),
 				quantity, reqQuantity)
 
 			GameTooltip_AddNormalLine(EmbeddedItemTooltip, criteriaString, wordWrap)
 			GameTooltip_ShowProgressBar(EmbeddedItemTooltip, 0, reqQuantity, quantity, progressString)
 			GameTooltip_AddNormalLine(EmbeddedItemTooltip, 
-				format("Victories required:\n   Solo:  %d\n    2v2:  %d\n    3v3:  %d\n    RBG: %d", 
+				format("Victories required:\n   Solo:  %d\n    2v2:  %d\n    3v3:  %d\n    RBG: %d",
 				victoriesLeftSolo, victoriesLeft2v2, victoriesLeft3v3, victoriesLeftrbg), wordWrap)
 			if rewardItemID then
 				GameTooltip_AddBlankLinesToTooltip(EmbeddedItemTooltip, 1)
@@ -39,7 +39,7 @@ hooksecurefunc(PVPQueueFrame.HonorInset.RatedPanel.SeasonRewardFrame, "UpdateToo
 				EmbeddedItemTooltip_SetItemByID(EmbeddedItemTooltip.ItemTooltip, rewardItemID)
 			end
 		end
-	end	
+	end
 
 	EmbeddedItemTooltip:Show()
 
